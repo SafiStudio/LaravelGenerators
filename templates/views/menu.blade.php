6017f8e8ff8170da302f017e3455aaf4848abbbd
Patryk Gałązka <patryk@safistudio.pl>
2017-02-07 09:58:59 +0100

    [update] CSS

--- templates/views/menu.blade.php
+++ templates/views/menu.blade.php
@@ -1,7 +1,7 @@
 <nav class="main-navigation">
     <ul>
         <li{!! strpos(Request::path(), 'admin/panel')!==false ? ' class="active"' : '' !!}>
-            <a href="{{ action('Admin\PanelController@index') }}"><i class="icon fa fa-dashboard"></i> Panel</a>
+            <a href="{{ action('Admin\PanelController@index') }}"><i class="icon fa fa-fw fa-dashboard"></i> Panel</a>
         </li>
     </ul>
 </nav>
